319ff7bc8a0b7c535df20e175d287a8c6bb2ce1d
admin <muhammadabdullah34907@gmail.com>
2023-10-23 15:03:27 +0500

    add gpu module with bugs

--- script.sh
+++ script.sh
@@ -4,6 +4,7 @@ git submodule sync
 git submodule update --init --recursive
 
 cp ../rnn-with-SELU/RNN.cpp aten/src/ATen/native/RNN.cpp
+cp ../rnn-with-SELU/RNN.cu aten/src/ATen/native/cuda/RNN.cu
 
 conda create --name pytorch-env -y
 conda activate pytorch-env
@@ -11,6 +12,7 @@ conda install cmake ninja -y
 pip3 install -r requirements.txt
 
 conda install mkl mkl-include -y
+conda install -c pytorch magma-cuda110 -y #replace 110 with your cuda version such as 118 for CUDA 11.8
 
 export _GLIBCXX_USE_CXX11_ABI=1
 export CMAKE_PREFIX_PATH=${CONDA_PREFIX:-"$(dirname $(which conda))/../"}
